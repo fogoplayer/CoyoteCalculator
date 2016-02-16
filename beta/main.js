4dae74f19df366dd22587a6a4d8c73536788d12c
fogoplayer <fogoplayer@gmail.com>
2016-02-15 21:07:26 -0500

    Update main.js

--- beta/main.js
+++ beta/main.js
@@ -1,5 +1,5 @@
 //Variables
-var version = "web.1.1.1";
+var version = "web.1.1.2";
 var helpPopUp = document.getElementById("helpPopUp");
 var inputBox = document.getElementById("input");
 var outputBox = document.getElementById("output");
