30bcc02efbd5580e22186dec22758da3eefda860
fogoplayer <fogoplayer@gmail.com>
2016-07-09 00:18:53 -0400

    Add files via upload

---
+++ scripts/variables.js
@@ -0,0 +1,29 @@
+//Variables
+var version = "web.2.2.1";
+var inputBox = document.getElementById("input");
+var outputBox = document.getElementById("output");
+var splashScreen = document.getElementById("splash");
+var largestSquare = "";
+var equals = document.getElementById("equals");
+var hamburger = document.getElementById("hamburger");
+var overlay = document.getElementById("overlay");
+var input = {
+  input:"",
+  indicator:"",
+  output:"",
+  operatorArray:[],
+  openParenthesesArray:[],
+  closeParenthesesArray:[]
+};
+
+//Algebra Variables
+var e = 0;
+var x = [];
+var verticalLine = [];
+var openParentheses = [];
+var closeParentheses = [];
+var operator = [];
+var y = "";
+var a = "";
+var exponent = [];
+/*This commnent exists solely to prevent ACE's "..." bug*/
